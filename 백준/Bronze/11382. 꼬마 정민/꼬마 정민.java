c1980f1a695fbf7fa2ec2495c367b33353ca2183
eunji0519 <107164622+eunji0519@users.noreply.github.com>
2023-06-04 20:00:14 +0900

    [Bronze V] Title: 꼬마 정민, Time: 208 ms, Memory: 17664 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		long A, B, C;
+		
+		A = sc.nextLong();
+		B = sc.nextLong();
+		C = sc.nextLong();
+		
+		sc.close();
+		
+		System.out.println(A + B + C);
+
+	}
+}
