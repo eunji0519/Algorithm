e20851e4c26da7b459646d485e36e86cf1735ebe
eunji0519 <107164622+eunji0519@users.noreply.github.com>
2023-06-06 17:55:24 +0900

    [Bronze V] Title: 시험 성적, Time: 208 ms, Memory: 17780 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -2,18 +2,24 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
+
 		Scanner sc = new Scanner(System.in);
-		int score;	
-		score = sc.nextInt();	
+		
+		int score;
+		
+		score = sc.nextInt();
+		
 		sc.close();
+
 		if (0 <= score && score <= 100) {
-			if(90 <= score && score <= 100) {
+			int divScore = score/10;
+			if(divScore == 9 || divScore == 10) {
 				System.out.println("A");
-			} else if (80 <= score && score < 90) {
+			} else if (divScore == 8) {
 				System.out.println("B");
-			} else if (70 <= score && score < 80) {
+			} else if (divScore == 7) {
 				System.out.println("C");
-			} else if (60 <= score && score < 70) {
+			} else if (divScore == 6) {
 				System.out.println("D");
 			} else {
 				System.out.println("F");
@@ -21,5 +27,6 @@ public class Main {
 		} else {
 			System.out.println("점수를 잘못 입력했습니다.");
 		}
+		
 	}
 }
