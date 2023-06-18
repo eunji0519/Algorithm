966485fdebee54d3a89cdcd3013b4fbb190ba5be
eunji0519 <107164622+eunji0519@users.noreply.github.com>
2023-06-18 21:03:05 +0900

    [Bronze IV] Title: 주사위 세개, Time: 128 ms, Memory: 14036 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.java
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -7,8 +7,6 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 
-//		같은 눈이 2개만 나오는 경우에는 1,000원+(같은 눈)×100원의 상금을 받게 된다. 
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String s = br.readLine();
 		StringTokenizer st = new StringTokenizer(s);
@@ -16,31 +14,34 @@ public class Main {
 		int b = Integer.parseInt(st.nextToken());
 		int c = Integer.parseInt(st.nextToken());
 		br.close();
+		// 상금
+		int price;
 		
 		if ((a > 0 && a < 7) && (b > 0 && b < 7) && (c > 0 && c < 7)) {
 //			같은 눈이 3개가 나오면 10,000원+(같은 눈)×1,000원의 상금을 받게 된다.
 			if ((a == b) && (b == c)) {
-				int num1 = 10000 + (a * 1000);
-				System.out.println(num1);
+				price = 10000 + (a * 1000);
+				System.out.println(price);
+//				같은 눈이 2개만 나오는 경우에는 1,000원+(같은 눈)×100원의 상금을 받게 된다. 
 			} else if ((a == b) || (b == c) || (c == a)) {
 				if(a==b) {
-					int num2 = 1000 + a * 100;
-					System.out.println(num2);
+					price = 1000 + a * 100;
+					System.out.println(price);
 				}
 				if(b==c) {
-					int num2 = 1000 + b * 100;
-					System.out.println(num2);
+					price = 1000 + b * 100;
+					System.out.println(price);
 				}
 				if(c==a) {
-					int num2 = 1000 + a * 100;
-					System.out.println(num2);
+					price = 1000 + a * 100;
+					System.out.println(price);
 				}
 			} else {
 //			모두 다른 눈이 나오는 경우에는 (그 중 가장 큰 눈)×100원의 상금을 받게 된다.
-				int num3 = a > b ? a : b;
-				num3 = num3 > c ? num3 : c;
-				num3 *= 100;
-				System.out.println(num3);
+				price = a > b ? a : b;
+				price = price > c ? price : c;
+				price *= 100;
+				System.out.println(price);
 			}
 		} else {
 			System.out.println("입력범위를 벗어났습니다.");
