e315fec182677cb7653199b0eb1c1b13700f19c5
eunji0519 <107164622+eunji0519@users.noreply.github.com>
2023-07-05 21:10:36 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 128 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -1,25 +1,29 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
+import java.io.OutputStreamWriter;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		 
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		 
-		 int N = Integer.parseInt(br.readLine());
-		 
-		 br.close();
-		 
-		 if ((N >= 1) && (N <= 100)) {
-			 for (int i = 1; i <= N; i++) {
-				for (int j = 1; j <= i; j++) {
-					System.out.print("*");
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+
+		br.close();
+
+		if ((N >= 1) && (N <= 100)) {
+			for (int i = 1; i <= N; i++) { // 행 만들기
+				for (int j = 1; j <= i; j++) { // 열 만들기
+					bw.write("*");
 				}
-				System.out.println();
+				bw.write("\n");
 			 }
+			bw.flush();
+			bw.close();
 		 } else {
 			 System.out.println("입력범위를 벗어났습니다");
 		 }
