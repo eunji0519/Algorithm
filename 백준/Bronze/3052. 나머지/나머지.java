f61012c2bddef44a132ddaef3767d5cf17342e74
eunji0519 <107164622+eunji0519@users.noreply.github.com>
2023-08-14 21:55:56 +0900

    [Bronze II] Title: 나머지, Time: 128 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int[] arr = new int[42];
+		int cnt = 0;
+
+		for (int i = 0; i < 10; i++) {
+			int N = Integer.parseInt(br.readLine());
+			int mod = N % 42;
+			arr[mod]++;
+		}
+
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] > 0)
+				cnt++;
+		}
+		System.out.println(cnt);
+	}
+
+}
